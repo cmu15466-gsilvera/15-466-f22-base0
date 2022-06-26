b4f2538c0cba4287584f686fb13bb42462517986
GustavoSilvera <grs5382@gmail.com>
2022-06-26 12:43:18 -0700

    adding rotating direction triangle

--- Gravector.cpp
+++ Gravector.cpp
@@ -11,11 +11,6 @@
 Gravector::Gravector()
 {
 
-    // set up trail as if ball has been here for 'forever':
-    ball_trail.clear();
-    ball_trail.emplace_back(ball, trail_length);
-    ball_trail.emplace_back(ball, 0.0f);
-
     //----- allocate OpenGL resources -----
     { // vertex buffer:
         glGenBuffers(1, &vertex_buffer);
@@ -125,7 +120,8 @@ bool Gravector::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size
         // convert mouse from window pixels (top-left origin, +y is down) to clip space ([-1,1]x[-1,1], +y is up):
         glm::vec2 clip_mouse = glm::vec2((evt.motion.x + 0.5f) / window_size.x * 2.0f - 1.0f,
                                          (evt.motion.y + 0.5f) / window_size.y * -2.0f + 1.0f);
-        left_paddle.y = (clip_to_court * glm::vec3(clip_mouse, 1.0f)).y;
+        direction_heading = glm::atan(clip_mouse.y, clip_mouse.x);
+        return true;
     }
 
     return false;
@@ -136,89 +132,14 @@ void Gravector::update(float elapsed)
 
     static std::mt19937 mt; // mersenne twister pseudo-random number generator
 
-    //----- paddle update -----
-
-    { // right player ai:
-        ai_offset_update -= elapsed;
-        if (ai_offset_update < elapsed)
-        {
-            // update again in [0.5,1.0) seconds:
-            ai_offset_update = (mt() / float(mt.max())) * 0.5f + 0.5f;
-            ai_offset = (mt() / float(mt.max())) * 2.5f - 1.25f;
-        }
-        if (right_paddle.y < ball.y + ai_offset)
-        {
-            right_paddle.y = std::min(ball.y + ai_offset, right_paddle.y + 2.0f * elapsed);
-        }
-        else
-        {
-            right_paddle.y = std::max(ball.y + ai_offset, right_paddle.y - 2.0f * elapsed);
-        }
-    }
-
-    // clamp paddles to court:
-    right_paddle.y = std::max(right_paddle.y, -court_radius.y + paddle_radius.y);
-    right_paddle.y = std::min(right_paddle.y, court_radius.y - paddle_radius.y);
-
-    left_paddle.y = std::max(left_paddle.y, -court_radius.y + paddle_radius.y);
-    left_paddle.y = std::min(left_paddle.y, court_radius.y - paddle_radius.y);
-
     //----- ball update -----
 
     // speed of ball doubles every four points:
-    float speed_multiplier = 4.0f * std::pow(2.0f, (left_score + right_score) / 4.0f);
+    float speed_multiplier = 4.0f;
 
-    // velocity cap, though (otherwise ball can pass through paddles):
     speed_multiplier = std::min(speed_multiplier, 10.0f);
 
     ball += elapsed * speed_multiplier * ball_velocity;
-
-    //---- collision handling ----
-
-    // paddles:
-    auto paddle_vs_ball = [this](glm::vec2 const &paddle) {
-        // compute area of overlap:
-        glm::vec2 min = glm::max(paddle - paddle_radius, ball - ball_radius);
-        glm::vec2 max = glm::min(paddle + paddle_radius, ball + ball_radius);
-
-        // if no overlap, no collision:
-        if (min.x > max.x || min.y > max.y)
-            return;
-
-        if (max.x - min.x > max.y - min.y)
-        {
-            // wider overlap in x => bounce in y direction:
-            if (ball.y > paddle.y)
-            {
-                ball.y = paddle.y + paddle_radius.y + ball_radius.y;
-                ball_velocity.y = std::abs(ball_velocity.y);
-            }
-            else
-            {
-                ball.y = paddle.y - paddle_radius.y - ball_radius.y;
-                ball_velocity.y = -std::abs(ball_velocity.y);
-            }
-        }
-        else
-        {
-            // wider overlap in y => bounce in x direction:
-            if (ball.x > paddle.x)
-            {
-                ball.x = paddle.x + paddle_radius.x + ball_radius.x;
-                ball_velocity.x = std::abs(ball_velocity.x);
-            }
-            else
-            {
-                ball.x = paddle.x - paddle_radius.x - ball_radius.x;
-                ball_velocity.x = -std::abs(ball_velocity.x);
-            }
-            // warp y velocity based on offset from paddle center:
-            float vel = (ball.y - paddle.y) / (paddle_radius.y + ball_radius.y);
-            ball_velocity.y = glm::mix(ball_velocity.y, vel, 0.75f);
-        }
-    };
-    paddle_vs_ball(left_paddle);
-    paddle_vs_ball(right_paddle);
 
     // court walls:
     if (ball.y > court_radius.y - ball_radius.y)
@@ -244,7 +165,6 @@ void Gravector::update(float elapsed)
         if (ball_velocity.x > 0.0f)
         {
             ball_velocity.x = -ball_velocity.x;
-            left_score += 1;
         }
     }
     if (ball.x < -court_radius.x + ball_radius.x)
@@ -253,26 +173,7 @@ void Gravector::update(float elapsed)
         if (ball_velocity.x < 0.0f)
         {
             ball_velocity.x = -ball_velocity.x;
-            right_score += 1;
         }
-    }
-
-    //----- gradient trails -----
-
-    // age up all locations in ball trail:
-    for (auto &t : ball_trail)
-    {
-        t.z += elapsed;
-    }
-    // store fresh location at back of ball trail:
-    ball_trail.emplace_back(ball, 0.0f);
-
-    // trim any too-old locations from back of trail:
-    // NOTE: since trail drawing interpolates between points, only removes back element if second-to-back element is too
-    // old:
-    while (ball_trail.size() >= 2 && ball_trail[1].z > trail_length)
-    {
-        ball_trail.pop_front();
     }
 }
 
@@ -282,7 +183,7 @@ void Gravector::draw(glm::uvec2 const &drawable_size)
 #define HEX_TO_U8VEC4(HX) (glm::u8vec4((HX >> 24) & 0xff, (HX >> 16) & 0xff, (HX >> 8) & 0xff, (HX)&0xff))
     const glm::u8vec4 bg_color = HEX_TO_U8VEC4(0x193b59ff);
     const glm::u8vec4 fg_color = HEX_TO_U8VEC4(0xf2d2b6ff);
-    const glm::u8vec4 shadow_color = HEX_TO_U8VEC4(0xf2ad94ff);
+    // const glm::u8vec4 shadow_color = HEX_TO_U8VEC4(0xf2ad94ff);
     const std::vector<glm::u8vec4> trail_colors = {
         HEX_TO_U8VEC4(0xf2ad9488),
         HEX_TO_U8VEC4(0xf2897288),
@@ -292,7 +193,6 @@ void Gravector::draw(glm::uvec2 const &drawable_size)
 
     // other useful drawing constants:
     const float wall_radius = 0.05f;
-    const float shadow_offset = 0.07f;
     const float padding = 0.14f; // padding between outside of walls and edge of window
 
     //---- compute vertices to draw ----
@@ -312,70 +212,26 @@ void Gravector::draw(glm::uvec2 const &drawable_size)
         vertices.emplace_back(glm::vec3(center.x - radius.x, center.y + radius.y, 0.0f), color, glm::vec2(0.5f, 0.5f));
     };
 
-    // shadows for everything (except the trail):
+    // inline helper function for triangle drawing:
+    auto draw_triangle = [&vertices](glm::vec2 const &center, const float radius, const float heading,
+                                     glm::u8vec4 const &color) {
+        /// NOTE: heading is in radians!
+        const float bottomLeft = heading + 2 * M_PI / 3;
+        const float bottomRight = heading + 4 * M_PI / 3;
+        const float Up = heading;
 
-    glm::vec2 s = glm::vec2(0.0f, -shadow_offset);
-
-    draw_rectangle(glm::vec2(-court_radius.x - wall_radius, 0.0f) + s,
-                   glm::vec2(wall_radius, court_radius.y + 2.0f * wall_radius), shadow_color);
-    draw_rectangle(glm::vec2(court_radius.x + wall_radius, 0.0f) + s,
-                   glm::vec2(wall_radius, court_radius.y + 2.0f * wall_radius), shadow_color);
-    draw_rectangle(glm::vec2(0.0f, -court_radius.y - wall_radius) + s, glm::vec2(court_radius.x, wall_radius),
-                   shadow_color);
-    draw_rectangle(glm::vec2(0.0f, court_radius.y + wall_radius) + s, glm::vec2(court_radius.x, wall_radius),
-                   shadow_color);
-    draw_rectangle(left_paddle + s, paddle_radius, shadow_color);
-    draw_rectangle(right_paddle + s, paddle_radius, shadow_color);
-    draw_rectangle(ball + s, ball_radius, shadow_color);
-
-    // ball's trail:
-    if (ball_trail.size() >= 2)
-    {
-        // start ti at second element so there is always something before it to interpolate from:
-        std::deque<glm::vec3>::iterator ti = ball_trail.begin() + 1;
-        // draw trail from oldest-to-newest:
-        constexpr uint32_t STEPS = 20;
-        // draw from [STEPS, ..., 1]:
-        for (uint32_t step = STEPS; step > 0; --step)
-        {
-            // time at which to draw the trail element:
-            float t = step / float(STEPS) * trail_length;
-            // advance ti until 'just before' t:
-            while (ti != ball_trail.end() && ti->z > t)
-                ++ti;
-            // if we ran out of recorded tail, stop drawing:
-            if (ti == ball_trail.end())
-                break;
-            // interpolate between previous and current trail point to the correct time:
-            glm::vec3 a = *(ti - 1);
-            glm::vec3 b = *(ti);
-            glm::vec2 at = (t - a.z) / (b.z - a.z) * (glm::vec2(b) - glm::vec2(a)) + glm::vec2(a);
-
-            // look up color using linear interpolation:
-            // compute (continuous) index:
-            float c = (step - 1) / float(STEPS - 1) * trail_colors.size();
-            // split into an integer and fractional portion:
-            int32_t ci = int32_t(std::floor(c));
-            float cf = c - ci;
-            // clamp to allowable range (shouldn't ever be needed but good to think about for general interpolation):
-            if (ci < 0)
-            {
-                ci = 0;
-                cf = 0.0f;
-            }
-            if (ci > int32_t(trail_colors.size()) - 2)
-            {
-                ci = int32_t(trail_colors.size()) - 2;
-                cf = 1.0f;
-            }
-            // do the interpolation (casting to floating point vectors because glm::mix doesn't have an overload for u8
-            // vectors):
-            glm::u8vec4 color = glm::u8vec4(glm::mix(glm::vec4(trail_colors[ci]), glm::vec4(trail_colors[ci + 1]), cf));
-
-            // draw:
-            draw_rectangle(at, ball_radius, color);
-        }
-    }
+        // bottom left vertex
+        vertices.emplace_back(
+            glm::vec3(center.x + glm::cos(bottomLeft) * radius, center.y + glm::sin(bottomLeft) * radius, 0.0f), color,
+            glm::vec2(0.5f, 0.5f));
+        // bottom right vertex
+        vertices.emplace_back(
+            glm::vec3(center.x + glm::cos(bottomRight) * radius, center.y + glm::sin(bottomRight) * radius, 0.0f),
+            color, glm::vec2(0.5f, 0.5f));
+        // top vertex (colored red)
+        vertices.emplace_back(glm::vec3(center.x + glm::cos(Up) * radius, center.y + glm::sin(Up) * radius, 0.0f),
+                              glm::u8vec4(255, 0, 0, 255), glm::vec2(0.5f, 0.5f));
+    };
 
     // solid objects:
 
@@ -387,35 +243,33 @@ void Gravector::draw(glm::uvec2 const &drawable_size)
     draw_rectangle(glm::vec2(0.0f, -court_radius.y - wall_radius), glm::vec2(court_radius.x, wall_radius), fg_color);
     draw_rectangle(glm::vec2(0.0f, court_radius.y + wall_radius), glm::vec2(court_radius.x, wall_radius), fg_color);
 
-    // paddles:
-    draw_rectangle(left_paddle, paddle_radius, fg_color);
-    draw_rectangle(right_paddle, paddle_radius, fg_color);
-
     // ball:
     draw_rectangle(ball, ball_radius, fg_color);
 
-    // scores:
-    glm::vec2 score_radius = glm::vec2(0.1f, 0.1f);
-    for (uint32_t i = 0; i < left_score; ++i)
-    {
-        draw_rectangle(glm::vec2(-court_radius.x + (2.0f + 3.0f * i) * score_radius.x,
-                                 court_radius.y + 2.0f * wall_radius + 2.0f * score_radius.y),
-                       score_radius, fg_color);
-    }
-    for (uint32_t i = 0; i < right_score; ++i)
-    {
-        draw_rectangle(glm::vec2(court_radius.x - (2.0f + 3.0f * i) * score_radius.x,
-                                 court_radius.y + 2.0f * wall_radius + 2.0f * score_radius.y),
-                       score_radius, fg_color);
-    }
+    draw_triangle(triangle, 0.5f, direction_heading, fg_color);
+
+    // // scores:
+    // glm::vec2 score_radius = glm::vec2(0.1f, 0.1f);
+    // for (uint32_t i = 0; i < left_score; ++i)
+    // {
+    //     draw_rectangle(glm::vec2(-court_radius.x + (2.0f + 3.0f * i) * score_radius.x,
+    //                              court_radius.y + 2.0f * wall_radius + 2.0f * score_radius.y),
+    //                    score_radius, fg_color);
+    // }
+    // for (uint32_t i = 0; i < right_score; ++i)
+    // {
+    //     draw_rectangle(glm::vec2(court_radius.x - (2.0f + 3.0f * i) * score_radius.x,
+    //                              court_radius.y + 2.0f * wall_radius + 2.0f * score_radius.y),
+    //                    score_radius, fg_color);
+    // }
 
     //------ compute court-to-window transform ------
 
     // compute area that should be visible:
     glm::vec2 scene_min =
         glm::vec2(-court_radius.x - 2.0f * wall_radius - padding, -court_radius.y - 2.0f * wall_radius - padding);
-    glm::vec2 scene_max = glm::vec2(court_radius.x + 2.0f * wall_radius + padding,
-                                    court_radius.y + 2.0f * wall_radius + 3.0f * score_radius.y + padding);
+    glm::vec2 scene_max =
+        glm::vec2(court_radius.x + 2.0f * wall_radius + padding, court_radius.y + 2.0f * wall_radius + padding);
 
     // compute window aspect ratio:
     float aspect = drawable_size.x / float(drawable_size.y);
--- Gravector.hpp
+++ Gravector.hpp
@@ -21,29 +21,17 @@ struct Gravector : Mode
     virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
     virtual void update(float elapsed) override;
     virtual void draw(glm::uvec2 const &drawable_size) override;
-
     //----- game state -----
 
     glm::vec2 court_radius = glm::vec2(7.0f, 5.0f);
-    glm::vec2 paddle_radius = glm::vec2(0.2f, 1.0f);
     glm::vec2 ball_radius = glm::vec2(0.2f, 0.2f);
-
-    glm::vec2 left_paddle = glm::vec2(-court_radius.x + 0.5f, 0.0f);
-    glm::vec2 right_paddle = glm::vec2(court_radius.x - 0.5f, 0.0f);
 
     glm::vec2 ball = glm::vec2(0.0f, 0.0f);
     glm::vec2 ball_velocity = glm::vec2(-1.0f, 0.0f);
 
-    uint32_t left_score = 0;
-    uint32_t right_score = 0;
-
-    float ai_offset = 0.0f;
-    float ai_offset_update = 0.0f;
-
-    //----- pretty gradient trails -----
-
-    float trail_length = 1.3f;
-    std::deque<glm::vec3> ball_trail; // stores (x,y,age), oldest elements first
+    //--- direction vector
+    float direction_heading = 0;
+    glm::vec2 triangle = glm::vec2(0.0f, 0.0f);
 
     //----- opengl assets / helpers ------
 
@@ -75,5 +63,4 @@ struct Gravector : Mode
     // matrix that maps from clip coordinates to court-space coordinates:
     glm::mat3x2 clip_to_court = glm::mat3x2(1.0f);
     // computed in draw() as the inverse of OBJECT_TO_CLIP
-    // (stored here so that the mouse handling code can use it to position the paddle)
 };
